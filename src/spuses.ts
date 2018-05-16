fdb483a2fe8734e06af625c8043bd1b5659dc0bc
SPGoding <SPGoding@outlook.com>
2018-05-16 13:56:14 +0800

    Update spuses.ts

--- src/spuses.ts
+++ src/spuses.ts
@@ -41,10 +41,52 @@ export default class Spuses {
             'advancement revoke %0 everything'
         ], [
             'advancement test %entity %string',
-            'execute if entity %0$adv%1'
+            'execute if entity %0$addAdvancement%1'
         ], [
             'advancement test %entity %string %string',
-            'execute if entity %0$adv%1%2'
+            'execute if entity %0$addAdvancement%1%2'
+        ], [
+            'ban %entity',
+            'ban %0'
+        ], [
+            'ban %entity %string',
+            'ban %0 %1'
+        ], [
+            'ban-ip %entity',
+            'ban-ip %0'
+        ], [
+            'ban-ip %entity %string',
+            'ban-ip %0 %1'
+        ], [
+            'ban-ip %string',
+            'ban-ip %0'
+        ], [
+            'ban-ip %string %string',
+            'ban-ip %0 %1'
+        ], [
+            'banlist %string',
+            'banlist %0'
+        ], [
+            'blockdata %position %nbt',
+            'data merge block %0 %1'
+        ], [
+            'clear',
+            'clear'
+        ], [
+            'clear %entity',
+            'clear %0'
+        ], [
+            'clear %entity %item',
+            'clear %0 %1'
+        ], [
+            'clear %entity %itemWithData',
+            'clear %0 %1'
+        ], [
+            'clear %entity %itemWithData %number',
+            'clear %0 %1 %2 %'
+        ], [
+            'clear %entity %itemWithData %number %nbt',
+            'clear %0$addNbt%3 %1 %2'
         ]
         ])
 }
