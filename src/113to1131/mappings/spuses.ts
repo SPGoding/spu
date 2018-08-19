9ce24b1493dd086b5fe8042fedc6a3c889b43bad
SPGoding <SPGoding@outlook.com>
2018-08-20 00:25:42 +0800

    :hankey:try to support 1..13.1.

--- src/113to1131/mappings/spuses.ts
+++ src/113to1131/mappings/spuses.ts
@@ -1,6 +1,10 @@
 export default class Spuses {
     public static pairs = new Map([
-        ['scoreboard players operation %word %word \\%= %word %word', ''],
+        [
+            'scoreboard players operation %word %word \\%= %word %word', 
+            'scoreboard players operation $0 $1 \\%= $2 $3\n' + 
+            'execute if score $0 $1 matches ..0 run scoreboard players '
+        ],
         ['%string', '%0']
     ])
 }
