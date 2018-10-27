2f8e15b366393c9e37847e53cf05112ee423c104
SPGoding <SPGoding@outlook.com>
2018-10-27 23:02:02 +0800

    :bug:Fix 18to19.

---
+++ src/18to19/mappings/mapping.ts
@@ -0,0 +1,11 @@
+export interface Number_String {
+    0: number
+    1: string
+}
+
+export interface Number_Number_String_StringArray {
+    0: number
+    1: number
+    2: string
+    3: string[]
+}
