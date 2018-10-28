f5bb3e81e14173dfdddff35e6cc43f8edbd690c8
SPGoding <SPGoding@outlook.com>
2018-10-28 10:27:21 +0800

    :heankey:Stash x 4.

--- src/utils/wheel_chief/wheel_chief.ts
+++ src/utils/wheel_chief/wheel_chief.ts
@@ -1,4 +1,4 @@
-import { BrigadierBoolParser, BrigadierLiteralParser } from "./argument_parsers";
+import { BrigadierBoolParser, BrigadierLiteralParser } from './argument_parsers'
 
 export interface CommandsNode {
     type: 'root' | 'literal' | 'argument'
@@ -6,24 +6,26 @@ export interface CommandsNode {
         [nodeName: string]: CommandsNode
     }
     parser?: string
-    properties?: object
+    properties?: {
+        [propertyName: string]: string
+    }
     executable?: boolean
-    redirect?: string[]
+    redirect?: string
+    spu_script?: string
 }
 
 export interface Command {
-    nodes: Node[]
+    args: string[]
     spuScript: string
 }
 
-export interface Node {
-    name: string
-    value: string
-    parser: string
-    deltaIndex: number
+export interface ParseResult {
+    command: Command
+    index: number
+    splited: string[]
 }
 
-/** 
+/**
  * WheelChief
  * 「旅长」命令解析系统的轮子 —— 「轮长」命令解析系统
  */
@@ -35,31 +37,87 @@ export default class WheelChief {
         }
     }
 
-    private static parseNode(args: string[], argIndex: number, nodeName: string, nodes: CommandsNode): object {
+    private static parseNode(
+        input: ParseResult,
+        nodeName: string,
+        nodes: CommandsNode,
+        rootNode: CommandsNode
+    ): ParseResult {
+        let result: ParseResult = {
+            command: { args: input.command.args.slice(0), spuScript: input.command.spuScript },
+            index: input.index,
+            splited: input.splited.slice(0)
+        }
         if (nodes.type === 'root') {
             if (nodes.children) {
                 for (const name in nodes.children) {
                     if (nodes.children.hasOwnProperty(name)) {
                         const child = nodes.children[name]
-                        return this.parseNode(args, argIndex, name, child)
+                        try {
+                            result = this.parseNode(result, name, child, rootNode)
+                        } catch {
+                            continue
+                        }
                     }
                 }
-                throw ''
             } else {
-                throw `Expected 'children' for root node.`
+                throw `Expected 'children' for the root node.`
             }
         } else if (nodes.type === 'literal') {
-            if (nodeName === args[argIndex]) {
-                return { argIndex: argIndex++ }
+            if (nodeName === input.splited[input.index]) {
+                result.command.args.push(nodeName)
+                result.index += 1
+                if (result.index >= input.splited.length) {
+                    if (nodes.executable) {
+                        if (nodes.spu_script) {
+                            result.command.spuScript = nodes.spu_script
+                        }
+                    } else {
+                        throw `Expected executable command.`
+                    }
+                } else {
+                    if (nodes.children) {
+                        let operated = false
+                        for (const name in nodes.children) {
+                            if (nodes.children.hasOwnProperty(name)) {
+                                const child = nodes.children[name]
+                                try {
+                                    result = WheelChief.parseNode(result, name, child, rootNode)
+                                    operated = true
+                                } catch {
+                                    continue
+                                }
+                            }
+                        }
+                        if (!operated) {
+                            throw `Can't match.`
+                        }
+                    } else {
+                        throw `Expected 'EndOfCommand'.`
+                    }
+                }
             } else {
-                throw ''
+                throw `Expected literal '${nodeName}' but get '${input.splited[input.index]}'.`
+            }
+        } else if (nodes.type === 'argument') {
+            if (nodes.parser) {
+                let canBeParsed = WheelChief.canBeParsed(input.splited[input.index], nodes.parser, nodes.properties)
+            } else {
+                throw `Expected 'parser' for the argument node.`
             }
         } else {
-            throw ''
+            throw `Unknown type: '${nodes.type}'. Can be one of the following values: 'root', 'literal' and 'argument'.`
         }
+        return result
     }
 
-    public canBeParsed(parser: string, value: string) {
-
-    }
+    public static canBeParsed(
+        value: string,
+        parser: string,
+        properties:
+            | {
+                  [propertyName: string]: string
+              }
+            | undefined
+    ) {}
 }
