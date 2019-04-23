6bcea6c81368c4fb7bad2553bae196b78cb9b580
SPGoding <SPGoding@outlook.com>
2019-04-23 21:54:21 +0800

    :truck: Move out to lib.

---
+++ lib/to113/mappings/items.d.ts
@@ -0,0 +1,39 @@
+import { NbtCompound, NbtString, NbtShort, NbtInt, NbtByte, NbtValue } from '../../utils/nbt/nbt';
+import { Number_String } from './mapping';
+export declare class StdItem1_12 {
+    private readonly name;
+    private readonly data;
+    private readonly tag;
+    private readonly count;
+    private readonly slot;
+    constructor(name: string, data: number, tag: NbtCompound, count?: NbtValue, slot?: NbtValue);
+    getName(): string;
+    getCount(): NbtCompound | import("../../../src/utils/nbt/nbt").NbtList | import("../../../src/utils/nbt/nbt").NbtByteArray | import("../../../src/utils/nbt/nbt").NbtIntArray | import("../../../src/utils/nbt/nbt").NbtLongArray | NbtByte | NbtShort | NbtInt | import("../../../src/utils/nbt/nbt").NbtLong | import("../../../src/utils/nbt/nbt").NbtFloat | import("../../../src/utils/nbt/nbt").NbtDouble | NbtString | undefined;
+    getSlot(): NbtCompound | import("../../../src/utils/nbt/nbt").NbtList | import("../../../src/utils/nbt/nbt").NbtByteArray | import("../../../src/utils/nbt/nbt").NbtIntArray | import("../../../src/utils/nbt/nbt").NbtLongArray | NbtByte | NbtShort | NbtInt | import("../../../src/utils/nbt/nbt").NbtLong | import("../../../src/utils/nbt/nbt").NbtFloat | import("../../../src/utils/nbt/nbt").NbtDouble | NbtString | undefined;
+    getData(): number;
+    getTag(): NbtCompound;
+    getNominal(): string;
+}
+export declare class StdItem1_13 {
+    private readonly name;
+    private readonly tag;
+    private readonly count;
+    private readonly slot;
+    constructor(name: string, nbt: NbtCompound, count?: NbtValue, slot?: NbtValue);
+    getName(): string;
+    getTag(): NbtCompound;
+    getNominal(): string;
+    getNbt(): NbtCompound;
+    hasTag(): boolean;
+}
+export default class Items {
+    static std112(id?: number, name?: string, data?: number, tag?: string, nbt?: string): StdItem1_12;
+    static to113(std: StdItem1_12): StdItem1_13;
+    static toNominalColor(input: number, suffix: string): string;
+    static NumericID112_NominalID112: Number_String[];
+    static Nominal112_NominalID113: string[][];
+    static DamagableItems: string[];
+    static MapItems: string[];
+    static SpawnEgges: string[];
+    static NumericColor_NominalColor: string[][];
+}
--- lib/utils/utils.d.ts
+++ lib/utils/utils.d.ts
@@ -1,8 +1,8 @@
 export declare function isNumeric(num: any): boolean;
 export declare function isWhiteSpace(char: string): boolean;
 export declare type NbtFormat = 'before 1.12' | 'after 1.12' | 'after 1.14';
-export declare function getNbtCompound(str: string, version?: NbtFormat): import("../../../../../../SPGoding/project/Typescript/spu/src/utils/nbt/nbt").NbtCompound;
-export declare function getNbtList(str: string, version?: NbtFormat): import("../../../../../../SPGoding/project/Typescript/spu/src/utils/nbt/nbt").NbtList;
+export declare function getNbtCompound(str: string, version?: NbtFormat): import("../../src/utils/nbt/nbt").NbtCompound;
+export declare function getNbtList(str: string, version?: NbtFormat): import("../../src/utils/nbt/nbt").NbtList;
 export declare function completeNamespace(input: string): string;
 export declare function getUuidLeastUuidMost(uuid: string): {
     L: number;
